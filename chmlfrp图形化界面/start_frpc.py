952e742dfe0345ac56cc08e1b16f7e891342144c
daijunhaoMinecraft <121751847+daijunhaoMinecraft@users.noreply.github.com>
2024-03-02 15:20:28 +0800

    更新 start_frpc.py

--- chmlfrp图形化界面/start_frpc.py
+++ chmlfrp图形化界面/start_frpc.py
@@ -2,8 +2,7 @@ import sys
 import requests
 import json
 import os
-from win10toast import ToastNotifier
-toaster = ToastNotifier()
+from plyer import notification
 import datetime
 import platform
 import psutil
@@ -30,9 +29,9 @@ try:
         f.write(json.loads(requests.get(f"https://panel.chmlfrp.cn/api/frpconfig.php?usertoken={get_token}&node={usertunnel_info[get_usertunnel_select]['node']}",verify=False,headers=headers).text)['message'])
         f.close()
 except Exception as e:
-    toaster.show_toast("出现错误",f"{e}", duration=10)
+    notification.notify(title='出现错误', message=f'{e}', app_icon=None, timeout=10)
     sys.exit()
 os.system("cls")
 os.system(f"start cmd /c \"@echo off&echo ChmlFrp日志信息 - {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}生成&echo.&echo ===========设备信息==============&echo.&echo 系统/系统版本:{platform.platform()}&echo 操作系统位数:{platform.architecture()[0]}&echo 处理器信息:{getcpu()}&echo 机带RAM:{round(psutil.virtual_memory().total / (1024**3))}GB&echo.&echo ===========隧道信息==============&echo.&echo 隧道ID:{usertunnel_info[get_usertunnel_select]['id']}&echo 隧道名称:{usertunnel_info[get_usertunnel_select]['name']}&echo 隧道类型:{usertunnel_info[get_usertunnel_select]['type']}&echo 内网IP:{usertunnel_info[get_usertunnel_select]['localip']}&echo 内网端口:{usertunnel_info[get_usertunnel_select]['nport']}&echo 外网端口/域名:{usertunnel_info[get_usertunnel_select]['dorp']}&echo 节点名称:{usertunnel_info[get_usertunnel_select]['node']}&echo 连接地址:{usertunnel_info[get_usertunnel_select]['ip']}&echo.&echo ===========FRPC输出==============&{pathx_pyinstaller}\\frpc.exe -u {sys.argv[1]} -p {usertunnel_info[get_usertunnel_select]['id']}&echo frpc已终止,按任意键退出&pause\"")
-toaster.show_toast(f"隧道正在启动--{usertunnel_info[get_usertunnel_select]['name']}",f"隧道ip地址:{usertunnel_info[get_usertunnel_select]['ip']}",duration=10)
+notification.notify(title = f"隧道正在启动--{usertunnel_info[get_usertunnel_select]['name']}",message = f"隧道ip地址:{usertunnel_info[get_usertunnel_select]['ip']}",app_icon = None,timeout = 10)
 sys.exit()
