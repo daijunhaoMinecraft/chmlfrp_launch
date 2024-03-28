9327c242e47f6512a6ee6c37305d1041a02b3021
daijunhao <121751847+daijunhaoMinecraft@users.noreply.github.com>
2024-03-28 21:27:49 +0800

    uqdate

--- chmlfrp图形化界面/start_frpc.py
+++ chmlfrp图形化界面/start_frpc.py
@@ -7,6 +7,8 @@ import datetime
 import platform
 import psutil
 import winreg
+token = sys.argv[1]
+id = sys.argv[2]
 #获取当前路径
 pathx = os.path.dirname(os.path.abspath(__file__))
 #忽略证书警告
@@ -22,21 +24,11 @@ headers = {
 }
 #获取当前exe执行路径
 pathx_pyinstaller = os.path.dirname(os.path.realpath(sys.argv[0]))
-#启动: 此文件 <token> <隧道序号-1>
+#启动: 此文件 <token> <隧道id>
 try:
-    get_token = sys.argv[1]
-    get_usertunnel_select = int(sys.argv[2])
-    usertunnel_info = json.loads(requests.get(f"https://panel.chmlfrp.cn/api/usertunnel.php?token={get_token}",headers=headers,verify=False).text)
-    try:
-        with open(f"{pathx_pyinstaller}\\frpc.ini",mode="w",encoding="utf-8") as f:
-            f.write(json.loads(requests.get(f"https://panel.chmlfrp.cn/api/frpconfig.php?usertoken={get_token}&node={usertunnel_info[get_usertunnel_select]['node']}",verify=False,headers=headers).text)['message'])
-            f.close()
-    except Exception as e:
-        notification.notify(title='出现错误', message=f'{e}', app_icon=f"{pathx}\\system_Error.ico", timeout=10)
-        sys.exit()
+    usertunnel_info = json.loads(requests.get(f"https://panel.chmlfrp.cn/api/tunnelinfo.php?id={id}",headers=headers,verify=False).text)
 except Exception as e:
     notification.notify(title='出现错误', message=f'{e}', app_icon=f"{pathx}\\system_Error.ico", timeout=10)
     sys.exit()
-os.system(f"start cmd /c \"@echo off&echo ChmlFrp日志信息 - {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}生成&echo.&echo ===========设备信息==============&echo.&echo 系统/系统版本:{platform.platform()}&echo 操作系统位数:{platform.architecture()[0]}&echo 处理器信息:{getcpu()}&echo 机带RAM:{round(psutil.virtual_memory().total / (1024**3))}GB&echo.&echo ===========隧道信息==============&echo.&echo 隧道ID:{usertunnel_info[get_usertunnel_select]['id']}&echo 隧道名称:{usertunnel_info[get_usertunnel_select]['name']}&echo 隧道类型:{usertunnel_info[get_usertunnel_select]['type']}&echo 内网IP:{usertunnel_info[get_usertunnel_select]['localip']}&echo 内网端口:{usertunnel_info[get_usertunnel_select]['nport']}&echo 外网端口/域名:{usertunnel_info[get_usertunnel_select]['dorp']}&echo 节点名称:{usertunnel_info[get_usertunnel_select]['node']}&echo 连接地址:{usertunnel_info[get_usertunnel_select]['ip']}&echo.&echo ===========FRPC输出==============&{pathx_pyinstaller}\\frpc.exe -u {sys.argv[1]} -p {usertunnel_info[get_usertunnel_select]['id']}&echo frpc已终止,按任意键退出&pause\"")
-notification.notify(title = f"隧道正在启动--{usertunnel_info[get_usertunnel_select]['name']}",message = f"隧道ip地址:{usertunnel_info[get_usertunnel_select]['ip']}",app_icon = f"{pathx}\\system_info.ico",timeout = 10)
-sys.exit()
+os.system(f"start cmd /c \"@echo off&echo ChmlFrp日志信息 - {datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')}生成&echo.&echo ===========设备信息==============&echo.&echo 系统/系统版本:{platform.platform()}&echo 操作系统位数:{platform.architecture()[0]}&echo 处理器信息:{getcpu()}&echo 机带RAM:{round(psutil.virtual_memory().total / (1024**3))}GB&echo.&echo ===========隧道信息==============&echo.&echo 隧道ID:{usertunnel_info['tunnel_id']}&echo 隧道名称:{usertunnel_info['tunnel_name']}&echo 隧道类型:{usertunnel_info['tunnel_type']}&echo 内网IP:{usertunnel_info['tunnel_localip']}&echo 内网端口:{usertunnel_info['tunnel_nport']}&echo 外网端口/域名:{usertunnel_info['tunnel_dorp']}&echo 节点名称:{usertunnel_info['name']}&echo 连接地址:{usertunnel_info['iparea']}&echo.&echo ===========FRPC输出==============&{pathx_pyinstaller}\\frpc.exe -u {token} -p {id}&echo frpc已终止,按任意键退出&pause\"")
+notification.notify(title = f"隧道正在启动--{usertunnel_info['tunnel_name']}",message = f"隧道ip地址:{usertunnel_info['iparea']}",app_icon = f"{pathx}\\system_info.ico",timeout = 10)
